4d233648d2442e0edd718568e959570d9305ebae
Helvio Junior <helvio_junior@hotmail.com>
2023-01-13 14:29:07 -0300

    CI/CD

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,32 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+### Configuration  
+impacket version:  
+Python version:  
+Target OS:  
+
+### Debug Output With Command String  
+i.e.  
+knowsmore --bloodhound --import-data 20230113045642_domains.json 
+```
+knowsmore --bloodhound --import-data 20230113045642_domains.json 
+
+ [+] Startup parameters
+     command line: knowsmore --bloodhound --import-data 20230113045642_domains.json 
+     module: bloodhound
+     database file: knowsmore.db
+  
+ [+] start time 2023-01-13 14:08:52
+ [!] error: BloodHound file is invalid 20230113045642_domains.json 
+
+```
+
+### Additional context  
+Space for additional context, investigative results, suspected issue.
--- setup.py
+++ setup.py
@@ -7,16 +7,22 @@ version = {}
 with open('knowsmore/meta.py') as f:
     exec(f.read(), version)
 
-setup(name='knowsmore',
-      version=version['__version__'],
-      description='Active Directory, BloodHound, NTDS hashes and Password Cracks correlation tool',
-      author='Hélvio Junior (M4v3r1ck)',
-      author_email='helvio_junior@hotmail.com',
-      url='https://github.com/helviojunior/knowsmore',
-      packages=find_packages(),
-      package_data={'knowsmore': ['resources/*']},
-      install_requires=['bs4>=0.0.1', 'colorama', 'clint>=0.5.1', 'tabulate>=0.9.0', 'Levenshtein>=0.20.9'],
-      entry_points= { 'console_scripts': [
-        'knowsmore=knowsmore.knowsmore:run',
-        ]}
-      )
+setup(  name='knowsmore',
+        version=version['__version__'],
+        description='Active Directory, BloodHound, NTDS hashes and Password Cracks correlation tool',
+        author='Hélvio Junior (M4v3r1ck)',
+        author_email='helvio_junior@hotmail.com',
+        url='https://github.com/helviojunior/knowsmore',
+        packages=find_packages(),
+        package_data={'knowsmore': ['resources/*']},
+        install_requires=['bs4>=0.0.1', 'colorama', 'clint>=0.5.1', 'tabulate>=0.9.0', 'Levenshtein>=0.20.9'],
+        readme="README.md",
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        entry_points= { 'console_scripts': [
+            'knowsmore=knowsmore.knowsmore:run',
+            ]}
+        )
