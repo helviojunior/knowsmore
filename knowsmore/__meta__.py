43483a68f6bac788ffc90732299a999b4bf65690
Helvio Junior <helvio_junior@hotmail.com>
2023-01-17 17:46:17 -0300

    Update build version

--- knowsmore/__meta__.py
+++ knowsmore/__meta__.py
@@ -1,8 +1,8 @@
-__version__ = '0.1.24'
+__version__ = '0.1.25'
 __title__ = "knowsmore"
 __description__ = "KnowsMore is a swiss army knife tool for pentesting Microsoft Active Directory (NTLM Hashes, BloodHound, NTDS and DCSync)."
 __url__ = "https://github.com/helviojunior/knowsmore"
-__build__ = 0x63c7045e
+__build__ = 0x63c70919
 __author__ = "Helvio Junior  (M4v3r1ck)"
 __author_email__ = "helvio.junior@sec4us.com.br"
 __license__ = "GPL-3.0"
