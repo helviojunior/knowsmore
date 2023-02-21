88b7e7a585d26505d655fcf22854bbb7c701ff5a
Helvio Junior <helvio_junior@hotmail.com>
2023-02-21 19:49:25 -0300

    Unit test to external utilization

--- tests/tests.py
+++ tests/tests.py
@@ -3,8 +3,10 @@ import codecs
 
 import pytest, sys
 
+from knowsmore.cmd.wordlist import WordList
 from knowsmore.knowsmore import KnowsMore
 from knowsmore.util.color import Color
+from knowsmore.util.tools import Tools
 
 
 def test_create_db():
@@ -36,3 +38,42 @@ def test_create_db():
         Color.pl('\n{!} {R}Exiting{W}\n')
 
         assert False
+
+# test to ensure to not broke scripts ansible
+#https://github.com/helviojunior/ansible-ad
+def test_wordlist():
+    try:
+
+        wlc = WordList()
+        wlc.small = False
+        wlc.name = "test"
+        wlc.max_size = len(wlc.name) + 5
+        wlc.min_size = 4
+        wlc.setup()
+        estimated_size = wlc.calculate()
+        Color.pl('{*} {W}Estimated bytes:{O} %s{W}' % Tools.sizeof_fmt(estimated_size, start_unit='K'))
+        max = 512 * 1024  # 1 GB
+        if estimated_size > max:
+            wlc.small = True
+            Color.pl('{*} {W}Estimated bytes:{O} %s{W}' % Tools.sizeof_fmt(estimated_size, start_unit='K'))
+
+        temp = [w for w in wlc.generate(wlc.name, 0)]
+
+        assert True
+        #sys.exit(0)
+    except Exception as e:
+        Color.pl('\n{!} {R}Error:{O} %s{W}' % str(e))
+
+        Color.pl('\n{!} {O}Full stack trace below')
+        from traceback import format_exc
+        Color.p('\n{!}    ')
+        err = format_exc().strip()
+        err = err.replace('\n', '\n{W}{!} {W}   ')
+        err = err.replace('  File', '{W}{D}File')
+        err = err.replace('  Exception: ', '{R}Exception: {O}')
+        Color.pl(err)
+
+        Color.pl('\n{!} {R}Exiting{W}\n')
+
+        assert False
+
